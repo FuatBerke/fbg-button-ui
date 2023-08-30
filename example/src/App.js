305eee4b0ddc910d28833a213c50639896d97fef
Fuat Berke Gültekin <fuatberkegultekin@gmail.com>
2023-08-30 08:54:17 +0300

    v1.0.3 | Secondary Button Color & Link Type

--- example/src/App.js
+++ example/src/App.js
@@ -10,40 +10,40 @@ const App = () => {
         <Button type='normal' color='primary' text='Button' />
         <Button type='outline' color='primary' text='Button' />
         <Button type='dashed' color='primary' text='Button' />
-        <Button type='text' color='primary' text='Button' />
         <Button type='link' color='primary' text='Button' />
+        <Button type='text' color='primary' text='Button' />
       </div>
       {/* SECONDARY */}
       <div>
         <Button type='normal' color='secondary' text='Button' />
         <Button type='outline' color='secondary' text='Button' />
         <Button type='dashed' color='secondary' text='Button' />
-        <Button type='text' color='secondary' text='Button' />
         <Button type='link' color='secondary' text='Button' />
+        <Button type='text' color='secondary' text='Button' />
       </div>
       {/* SUCCESS */}
       <div>
         <Button type='normal' color='success' text='Button' />
         <Button type='outline' color='success' text='Button' />
         <Button type='dashed' color='success' text='Button' />
-        <Button type='text' color='success' text='Button' />
         <Button type='link' color='success' text='Button' />
+        <Button type='text' color='success' text='Button' />
       </div>
       {/* WARNING */}
       <div>
         <Button type='normal' color='warning' text='Button' />
         <Button type='outline' color='warning' text='Button' />
         <Button type='dashed' color='warning' text='Button' />
-        <Button type='text' color='warning' text='Button' />
         <Button type='link' color='warning' text='Button' />
+        <Button type='text' color='warning' text='Button' />
       </div>
       {/* DANGER */}
       <div>
         <Button type='normal' color='danger' text='Button' />
         <Button type='outline' color='danger' text='Button' />
         <Button type='dashed' color='danger' text='Button' />
-        <Button type='text' color='danger' text='Button' />
         <Button type='link' color='danger' text='Button' />
+        <Button type='text' color='danger' text='Button' />
       </div>
     </>
   )
